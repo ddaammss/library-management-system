9c652f51289d342397794fb4f67d50b672ac6814
dams <ahwkt46@gmail.com>
2026-07-03 20:47:21 +0900

    Remove unused manager class initializations from main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
-#include "book/BookManager.h"
-#include "member/MemberManager.h"
-#include "loan/LoanManager.h"
-#include "search/Search.h"
+
 
 int main() {
-    BookManager bookManager;
-    MemberManager memberManager;
-    LoanManager loanManager;
-
     std::cout << "도서관 관리 시스템 시작" << std::endl;
 
     return 0;
